6fdc6b0e4da6c2524a0a906800f05f84501b3472
Jdbye <jdbye3@gmail.com>
2013-04-24 18:21:38 +0200

    Bootleg main menu items updated to match the ones translated previously
    Signed-off-by: Jdbye <jdbye3@gmail.com>

--- build_bootleg_power.bat
+++ build_bootleg_power.bat
@@ -1,7 +1,14 @@
+@echo off
+echo Deleting patched ROMs...
 del rom\power_patched.gba
 copy rom\power_2009trans.gba rom\power_patched.gba
+echo Compressing smallMenuGfx...
 Tools\GfxCompressor.exe asm\bin\SmallMenuGfx.bin asm\bin\SmallMenuGfx_Malias2.bin
+call batchcompressgfx.bat
+echo Applying ASM hacks...
 armips.exe asm\GFX.asm
 armips.exe asm\VariableWidthFont_Power.asm
+echo Inserting script...
 Atlas rom\power_patched.gba script\tf2_bootleg_script_autowrite.txt
+echo Done.
 pause
